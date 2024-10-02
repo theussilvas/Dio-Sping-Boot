6392bd9d02be96778b28de1c02c2c517af38401e
Matheus <silva.dantas2@escolar.ifrn.edu.br>
2024-10-02 00:59:26 -0300

    add

---
+++ ControleDeFluxo/src/controlefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,13 @@
+package controlefluxo;
+
+public class ParametrosInvalidosException extends Exception {
+	
+	public ParametrosInvalidosException() {
+		super();
+	}
+	
+	public ParametrosInvalidosException(String mensagem) {
+		super (mensagem);
+	}
+	
+}
